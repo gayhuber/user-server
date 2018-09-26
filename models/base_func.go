4c0704585425e4b5d334fed9f08654342ab76f01
gaofeifiy <5173180@qq.com>
2018-09-26 17:44:24 +0800

    新增两个 base 函数

--- models/base_func.go
+++ models/base_func.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"user-server/tools/hprose"
@@ -20,6 +21,18 @@ func init() {
 		Module: "Passport",
 		Class:  "Core\\MobileSmsCode",
 		Func:   "sendMobileSmsCode",
+	})
+
+	hprose.ServiceMp.AddMethod("quickMobileLogin", hprose.BaseClient{
+		Module: "Passport",
+		Class:  "Core\\Login",
+		Func:   "quickMobileLogin",
+	})
+
+	hprose.ServiceMp.AddMethod("getSimpleUserInfoById", hprose.BaseClient{
+		Module: "User",
+		Class:  "UserNew",
+		Func:   "getSimpleUserInfoById",
 	})
 }
 
@@ -56,20 +69,91 @@ func SendMobileSmsCode(mobile, countryCode, ip, Type string) error {
 
 	res, err := hprose.RemoteFunc("sendMobileSmsCode", arg)
 
+	if err != nil {
+		return err
+	}
+
 	// 这个变相转换一下输出, 用 json.Unmarshal 会出现map格式转换问题
+	// tmp := map[string]string{}
+	// for k, v := range res.(map[interface{}]interface{}) {
+	// 	key := fmt.Sprintf("%v", k)
+	// 	value := fmt.Sprintf("%v", v)
+	// 	tmp[key] = value
+	// }
+	tmp := respHandler(res)
+
+	if tmp["errorCode"] != "0" {
+		return errors.New(tmp["errorMsg"])
+	}
+	return nil
+}
+
+func respHandler(res interface{}) map[string]string {
 	tmp := map[string]string{}
 	for k, v := range res.(map[interface{}]interface{}) {
 		key := fmt.Sprintf("%v", k)
 		value := fmt.Sprintf("%v", v)
 		tmp[key] = value
 	}
+	return tmp
+}
+
+// QuickMobileLogin 手机验证码快速注册/登录
+func QuickMobileLogin(mobile, smsCode, countryCode string, sys int) (responseData string, err error) {
+	openKey, err := GetXyOpenKey(mobile)
+	if err != nil {
+		return
+	}
+
+	arg := args{
+		"mobile":      mobile,
+		"smsCode":     smsCode,
+		"countryCode": countryCode,
+		"key":         openKey,
+		"extInfo": args{
+			"sys": sys,
+		},
+		"lver":    "",
+		"version": "",
+	}
+
+	res, err := hprose.RemoteFunc("quickMobileLogin", arg)
+	fmt.Println("remote QuickMobileLogin:", res, err)
 
 	if err != nil {
-		return err
+		return
 	}
 
+	tmp := respHandler(res)
+
 	if tmp["errorCode"] != "0" {
-		return errors.New(tmp["errorMsg"])
+		err = errors.New(tmp["errorMsg"])
+		return
 	}
-	return nil
+	return tmp["responseData"], nil
+}
+
+func GetSimpleUserInfoById(uid int, returnUidAsKey bool, arrField []string) (info map[string]string, err error) {
+
+	arg := args{
+		"uid":            uid,
+		"returnUidAsKey": returnUidAsKey,
+		"arrField":       arrField,
+	}
+
+	res, err := hprose.RemoteFunc("getSimpleUserInfoById", arg)
+	fmt.Println("remote GetSimpleUserInfoById:", res, err)
+
+	if err != nil {
+		return
+	}
+
+	tmp := respHandler(res)
+
+	if tmp["errorCode"] != "0" {
+		err = errors.New(tmp["errorMsg"])
+		return
+	}
+	err = json.Unmarshal([]byte(tmp["responseData"]), &info)
+	return
 }
--- tools/hprose/client_test.go
+++ tools/hprose/client_test.go
@@ -1,6 +1,9 @@
 package hprose
 
 import (
+	// "encoding/json"
+	// "errors"
+	"fmt"
 	"testing"
 )
 
@@ -20,4 +23,54 @@ func TestCallback(t *testing.T) {
 	res, err := RemoteFunc("XyToken", args)
 
 	t.Log(res, err)
+}
+
+func TestCallback2(t *testing.T) {
+	ServiceMp.AddMethod("getSimpleUserInfoById", BaseClient{
+		Module: "User",
+		Class:  "UserNew",
+		Func:   "getSimpleUserInfoById",
+	})
+
+	field := []string{"uid", "user_name", "avatar", "login_mobile"}
+	resp, err := GetSimpleUserInfoById(20532239, false, field)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Println("result:", resp)
+}
+
+type args map[string]interface{}
+
+// GetSimpleUserInfoById 获取用户信息
+func GetSimpleUserInfoById(uid int, returnUidAsKey bool, arrField []string) (info map[string]string, err error) {
+
+	arg := args{
+		"uid":            uid,
+		"returnUidAsKey": returnUidAsKey,
+		"arrField":       arrField,
+	}
+
+	res, err := RemoteFunc("getSimpleUserInfoById", arg)
+	fmt.Println("remote GetSimpleUserInfoById:", res, err)
+
+	if err != nil {
+		return
+	}
+
+	tmp := respHandler(res)
+	fmt.Println("this is tmp:", tmp)
+	return tmp, nil
+}
+
+func respHandler(res interface{}) map[string]string {
+	tmp := map[string]string{}
+	for k, v := range res.(map[interface{}]interface{}) {
+		key := fmt.Sprintf("%v", k)
+		value := fmt.Sprintf("%v", v)
+		tmp[key] = value
+	}
+	return tmp
 }
